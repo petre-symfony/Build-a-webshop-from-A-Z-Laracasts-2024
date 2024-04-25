87188da84a905fa36742b6949a6339467c161381
petrero <petrero@gmail.com>
2024-04-25 10:54:19 +0300

    4.5 So at this time, none of our products have a featured image, and of course we want each product to have a featured image, so this is what we can show whenever we are listing our products, and of course we don't want to show all the images.  So if we head over back to our database here, you can see that we don't have any control on how to do this right now. So let's make sure that we change this. So instead of using this magic method, we're going to say Has, and we're going to provide the factory instead. So we're going to say Image, Factory, we're going to again say that we want to have three images. Next, we're going to call Sequence, and Sequence is basically a method that you can call which will be ran every time a new image is initiated, so this method will be called three times. And it accepts a closure that provides a sequence object, and what we basically want to do is whenever we're at the start of the sequence, we want to make it featured, or in other words, the first image that is created is going to be our featured image.  So we can return an array of attributes that we want to modify, so in this case we want to modify the Featured column on our model, and we're going to say Sequence, and you can see that it provides an index or a count, and in this case, whenever our index is set to zero, we want to make sure that it is featured. And if we refresh our database, and run a quick tinker, and we scroll down to our first image, you can see that our first image is featured, and our second image is not. So this is a pretty cool way to hook into the Factory, so of course you can use the Measure methods, but it doesn't give that much control, and whenever you need more granular control, you can simply call the Factory manually, like we've done just now, and it gives you all the Factory features that are available, so you can make any adjustments that you need.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
 use App\Models\Product;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -14,7 +16,9 @@ class DatabaseSeeder extends Seeder {
 	public function run(): void {
 		Product::factory(4)
 			->hasVariants(5)
-			->hasImages(3)
+			->has(Image::factory(3)->sequence(
+				fn(Sequence $sequence) => ['featured' => $sequence->index === 0]
+			))
 			->create();
 	}
 }
