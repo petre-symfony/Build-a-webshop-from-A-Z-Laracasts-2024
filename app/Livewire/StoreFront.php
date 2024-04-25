4545165d57ba3029094980e5c395315f584ce044
petrero <petrero@gmail.com>
2024-04-25 13:37:04 +0300

    5.6 Okay, so if we take a look at our example store, we can move on and start listing our products. So let's move back to our editor. Let's open up our storefronts component. And let's first start by fetching our products. So in this case, we want to query the database for all of our products.  And there are a few ways how we can do this, and I prefer to use computed properties. Computer properties allow you to do certain intensive tasks like fetching data, and LiveWire will ensure that no matter how often you call this method or try to attempt to read data from this computer property, it will always return the same data on a single request. So if you would call this method five times, it will always return the same data five times instead of querying the database five times in a row.  To use a computed property, you have to adhere to a certain syntax when naming your method. nSo we're going to start with that, and now the name of our properties, in this case products, and we'll end with property. Now this is a sort of magic method.  So inside our blade file, we can now do this, products, and it will actually reference this method. So let's simply do product query, and we'll get everything from the database. So now if we head to our blade file, we can give this a try and say for each, this product, and let's simply show the product name. So now if we switch to the browser and reload the page, you can see that we now get all these different products. And again, if you would call this five times, it will only execute that database query once.  Okay, so if we take a look at our example store, we know that we have a product image, we have the title of our product, and our price. And we need to, of course, do a little bit of styling as well. Now given there is plenty of work to do, we're not going to cover Tailwind CSS in depth in this course. If you'd like to learn more about Tailwind CSS, head over to tailwindcss.com, and there are a ton of resources on learning how you can use this framework.  In this course, we're going to apply some basic styling so our store looks pretty nice. But if you want to go further, definitely head over to the website of Tailwind CSS to learn more about advanced styling.  In our example store, we have three products per row. In our example, let's do four. We're going to create a grid, and we're going to use four columns. It should be okay. And we want maybe a gap of four.  So the styles weren't updating because I wasn't running speed at this time, so just make sure that you start it up.   npm run dev  And now when we switch back, we should get the correct results, and there we go. Our products are now across four columns. But it's probably best to add some sort of container. So if we switch back to our layout, we can simply head over to our main slot, and let's add a maximum width so we can center the content. Maybe do max-width to 2XL. Let's set the automatic margin on the horizontal axis, and you can see our products are centered. Maybe do 4XL. Let's leave it at that.  Next, go back to our storefront. We want to make sure that we have a little bit of margin at the top. Let's say 12. That looks okay.  So now we already have our title. We want to have our price and our image. So let's add some simple styling here. Let's make sure that our product title is emphasized a bit more. So let's make our font medium. Let's make the text large, make it look better.  Now below it, we want to have our price. Okay. We'll get to formatting the price later on. Let's change the color of our price a little bit. Let's say text gray, maybe 700. That looks fine. And let's make the font size a little bit smaller. Okay.

--- app/Livewire/StoreFront.php
+++ app/Livewire/StoreFront.php
@@ -2,9 +2,14 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class StoreFront extends Component {
+	public function getProductsProperty() {
+		return Product::query()->get();
+	}
+
 	public function render() {
 		return view('livewire.store-front');
 	}
--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,3 +1,8 @@
-<div>
-  {{-- The Master doesn't talk, he acts. --}}
+<div class="grid grid-cols-4 gap-4 mt-12">
+  @foreach($this->products as $product)
+    <div>
+      <h2 class="font-medium text-lg">{{ $product->name }}</h2>
+      <span class="text-gray-700 text-sm">{{ $product->price }}</span>
+    </div>
+  @endforeach
 </div>
