202fe8239b7bdc7c502880f337ca4154be6f6997
petrero <petrero@gmail.com>
2024-04-27 00:49:51 +0300

    6.2 Now we can head over to our product and we can implement a accessor for our right let's define a new method and let's call it price and that's going to return the attribute and now we can say return attribute make and there's a getter and a setter so in this case with a get we can use a closure or whenever we get the price from the database which is an integer we want to transform it into a money object but the value is going to be equal to the price from the database we're going to return a new money object so we're going to say return new money and the first argument is going to be the amount equal to the value from the database next we got the currency and let's set it to USD now when we switch with the browser we'll probably get an error as we're trying to echo out the price which is now a object and there we go if you press the page you can see that it cannot show the value because a string is expected but Money class is given   htmlspecialchars(): Argument #1 () must be of type string, Money\Money given

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Money\Money;
 
 class Product extends Model {
 	use HasFactory;
+
+	protected function price():Attribute {
+		return Attribute::make(
+			get: function (int $value) {
+				return new Money($value, new \Money\Currency('USD'));
+			}
+		);
+	}
 
 	public function variants(): HasMany {
 		return $this->hasMany(ProductVariant::class);
