1c5e942f5d6291c646c3cfdc797943df78bf19b8
petrero <petrero@gmail.com>
2024-04-27 20:46:17 +0300

    9.1 Add to Cart Validation  so let's continue by adding some validation whenever we want to add a product variant to our car but first let's make sure that we wire up our properties on our product component on this case we want to wire up the variant selector so to do this we can use wire model and this difference between a regular HTML form element normally you would use the name and do a post request now because we're using LiveWire, LiveWire does a bit of magic to do these things behind the scene and they instead use a different term which is model so we can change this into variant now if we would head over to the browser and would change this we will get an error and a little bit complain I am unable to set component data, the public property variant is not found on the component product

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -24,7 +24,7 @@
     </div>
 
     <div class="mt-4 space-y-4">
-      <select class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-800">
+      <select wire:model="variant" class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-800">
         @foreach($this->product->variants as $variant)
           <option value="{{ $variant->id }}">{{ $variant->size }} / {{ $variant->color }}</option>
         @endforeach
