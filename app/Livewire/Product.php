048f110b40883c639488c117a6074c007d19b27f
petrero <petrero@gmail.com>
2024-04-27 09:16:41 +0300

    7.4 Now if we would head back to our browser, we'll get an error because product is not defined yet. So let's open up our product component. Next, we'll call the mount method, and the mount method is called whenever the component is initialized.  So we'll only do it on an initial request. On any subsequential request, this method is not called. So we are using a Livewire page component, and this means that when we set it up with our routing, the mount method is automatically passed any arguments that we have defined inside our route.  So if we go back to our web, you can see that we named this product. You know what? Let's call this product ID. Now we can reference this here.  And if we take an example, if we would die and dump this and switch to a browser, you can see that this is one, and whenever we change this up here, it changes as well.

--- app/Livewire/Product.php
+++ app/Livewire/Product.php
@@ -5,6 +5,10 @@ namespace App\Livewire;
 use Livewire\Component;
 
 class Product extends Component {
+	public function mount($productId) {
+		dd($productId);
+	}
+
 	public function render() {
 		return view('livewire.product');
 	}
