2fd17eaabf0a09ec89eb04c8263eac2730053f0f
petrero <petrero@gmail.com>
2024-04-27 20:44:24 +0300

    8.2 So now whenever we click one of these smaller images, we want the following image to be swapped out.  So next we want to add a click handler to each of our images shown on the bottom side where we have the little thumbnails. So again, we can use Alpine, we can use the add click listener, which will trigger whenever someone clicks one of these images, and will basically tell Alpine to update the image property on the data object, and we want to change it to the path of the image that the user is clicking. This should do the trick. Whenever we click one of these images below, it should swap out with the image above.  As you can see, Alpine is very easy to use, and we make this dynamic with just a few changes. So whenever you're adding functionality, always consider, should I use Alpine or should I use Livewire?  Because whenever you use Livewire, you're going to make a request to the server. And that's fine, but you can always optimize it by reducing and minimizing the amount of requests you have to make to the server

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -10,7 +10,7 @@
     <div class="grid grid-cols-4 gap-4 my-4">
       @foreach($this->product->images as $image)
         <div class="rounded bg-white p-2 shadow">
-          <img src="/{{ $image->path }}"  alt="">
+          <img src="/{{ $image->path }}" @click="image = '/{{ $image->path }}'" alt="">
         </div>
       @endforeach
     </div>
