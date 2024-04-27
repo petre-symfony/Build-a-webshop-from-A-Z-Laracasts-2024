bac4cb51f30384830919a5a5c6b3a67703fced52
petrero <petrero@gmail.com>
2024-04-27 09:52:40 +0300

    7.8 So let's clean this up a bit. Let's add a white background. Let's add some padding and rounded borders, and also a little shadow. So that looks a bit better. I think on our storefront, we gave a top margin of 12, so let's apply this here as well.  Now we only have to clean up this little bit. I think we'll simply wrap this in a div, and let's move up this class. Up to the div above. Let's also make it a white background, give it some padding, some rounded corners, and a bit of shadow. That looks better.  Let's make sure that there's some spacing between the main image and our other images. Let's also give some bottom margins. Let's change this into MY12, so we get some space at the bottom as well. Change the UI. There we go. Now we have some room on the bottom as well.  Now let's improve our product information a little bit. So let's add a heading. And let's increase the font size, maybe three times xl. Yeah, that looks okay. Let's increase the font weight. It's also fine.  Next, we can add the product price. Again, this is automatically rendered because of our BladeStringable method that we defined earlier. There we go, we have the price. Let's increase the font size here as well a little bit. And let's turn down the intensity a little bit by making it text gray 700. Yeah, that looks great.  Next, we have our product description. Let's make sure that we have a little bit of room between the product name, price, and our description.

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,16 +1,23 @@
-<div class="grid grid-cols-2 gap-10">
-  <div>
-    <div>
+<div class="grid grid-cols-2 gap-10 my-12">
+  <div class="spac-y-4">
+    <div class="bg-white p-5 rounded-lg shadow">
       <img src="/{{ $this->product->image->path }}" alt="">
     </div>
 
-    <div class="grid grid-cols-4 gap-4">
+    <div class="grid grid-cols-4 gap-4 my-4">
       @foreach($this->product->images as $image)
-        <img src="/{{ $image->path }}" class="rounded" alt="">
+        <div class="rounded bg-white p-2 shadow">
+          <img src="/{{ $image->path }}"  alt="">
+        </div>
       @endforeach
     </div>
   </div>
   <div>
-    {{ $this->product->name }}
+    <h1 class="text-3xl font-medium">{{ $this->product->name }}</h1>
+    <div class="text-xl text-gray-700">{{ $this->product->price }}</div>
+
+    <div class="mt-5">
+      {{ $this->product->description }}
+    </div>
   </div>
 </div>
