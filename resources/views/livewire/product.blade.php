039976fe656a531916c23d5e97041dafb0efac1a
petrero <petrero@gmail.com>
2024-04-27 09:33:10 +0300

    7.7 And there you go, we can now see our images, but it's not aligned correctly, so let's fix that as well. And I need to make sure that we wrap this in another div.  Let's echo out our product name. Sorry. And there you go, we now have the image on the left-hand side, which contains our main image, and we have the other images, including our main image, listed below the main image.

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,11 +1,16 @@
 <div class="grid grid-cols-2 gap-10">
   <div>
-    <img src="/{{ $this->product->image->path }}" alt="">
+    <div>
+      <img src="/{{ $this->product->image->path }}" alt="">
+    </div>
+
+    <div class="grid grid-cols-4 gap-4">
+      @foreach($this->product->images as $image)
+        <img src="/{{ $image->path }}" class="rounded" alt="">
+      @endforeach
+    </div>
   </div>
-  
-  <div class="grid grid-cols-4 gap-4">
-    @foreach($this->product->images as $image)
-      <img src="/{{ $image->path }}" class="rounded" alt="">
-    @endforeach
+  <div>
+    {{ $this->product->name }}
   </div>
 </div>
