486709d9af53971e7c68e77365d97a9383575155
petrero <petrero@gmail.com>
2024-04-27 09:10:39 +0300

    7.3 So that's working, and now we can move on to showing the actual product data on our product page. If we take a look at our example store, you can see that we have a title, we have the price again, we have a description, we have our variants, we have an Add to Product card, and we have an image of our products.  So let's get going and start to implement this inside our project as well. So let's open up our product blade view, and let's kick off by creating a grid, and we'll do a two-column grid, give it quite a gap, maybe 10. Now on the left side of our grid, I want to have our product image and all the other product images as well.  Let's start off by creating our image, and let's reference our product image and our path. Now below the image, I want to have all the other images, so we can do for each and reference our images relationship. And let's make this a grid as well, let's do four images, and let's add a little gap, so we have our images, and we can reference the same examples above. In this case, we have our image model straight away, so we'll do image path. Let's make sure that we add a little bit of a rounded border. It should be fine.

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,3 +1,11 @@
-<div>
-  {{-- Do your work, then step back. --}}
+<div class="grid grid-cols-2 gap-10">
+  <div>
+    <img src="{{ $product->image->path }}" alt="">
+  </div>
+  
+  <div class="grid grid-cols-4 gap-4">
+    @foreach($product->images as $image)
+      <img src="{{ $image->path }}" class="rounded" alt="">
+    @endforeach
+  </div>
 </div>
