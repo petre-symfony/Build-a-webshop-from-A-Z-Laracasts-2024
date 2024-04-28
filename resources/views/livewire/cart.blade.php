db9462f9e63d119a8028a9fefe3c8d3d0588a957
petrero <petrero@gmail.com>
2024-04-28 17:24:33 +0300

    14.3 Now let's turn our cart into a table, so let's create a table heading, and we'll add a first column which is going to be equal to our product name, and our second column is going to contain our product quantity.
     Now let's add our table body, and inside for each loop we're going to add a row, and make sure that our columns match up with our table heading. So here we want to have our product name, so we'll still have to set up that inverse relationship, and here we'll have our quantity. If we head back, we can see that we have all of our products and our quantity listed.

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,5 +1,18 @@
 <div class="bg-white rounded-lg shadow p-5 mt-12">
-  @foreach($this->items as $item)
-    {{ $item->id }} - {{ $item->product_variant_id }} - {{ $item->quantity }}
-  @endforeach
+  <table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Quantity</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($this->items as $item)
+      <tr>
+        <td>{{ 'product' }}</td>
+        <td>{{ $item->quantity }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
 </div>
