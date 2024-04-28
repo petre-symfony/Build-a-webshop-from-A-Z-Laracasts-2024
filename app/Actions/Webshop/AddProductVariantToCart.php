0e70524b96f32b0b0be325e65fe27c263dbaf91e
petrero <petrero@gmail.com>
2024-04-28 19:01:00 +0300

    15.2 Next, something you might want to look into is, as you can see, we currently have a cap right here with size XL, color green. And if we add a couple, you can see that we actually get the same item multiple times. Instead, whenever a user adds an item two times, we want the quantity to be updated instead of having duplicate items.  So let's switch back to our editor, and let's visit our product component. So here we have our action, and if we head over to our action, then you can see that we're always creating a new item based on our variant ID. So let's change it.  So instead of calling the create method, I'm going to say first.create. Now we want Eloquent to look for our variant ID and check whether there is already an item in our part that matches this variant ID. So this is going to be the first array. And the second array contains any data that we want to set as well. So what we can do is we're going to say simply set our quantity to zero. And next, we're simply going to increment our quantity.  So let's check if this works. Let's add another arc. Yep, in this case. Let's add a few. Switch to our cart, and now you can see that our quantity has increased. Of course, we still have this remaining item from our test, so let's remove this. Now we can see that whenever we add a new product, let's do this one. Let's add a few. You can see that now the quantity is updated instead of adding a new individual card item.

--- app/Actions/Webshop/AddProductVariantToCart.php
+++ app/Actions/Webshop/AddProductVariantToCart.php
@@ -6,9 +6,13 @@ use App\Factories\CartFactory;
 
 class AddProductVariantToCart {
 	public function add($variantId) {
-		CartFactory::make()->items()->create([
-			'product_variant_id' => $variantId,
-			'quantity' => 1
-		]);;
+		CartFactory::make()->items()->firstOrCreate(
+			[
+				'product_variant_id' => $variantId
+			],
+			[
+				'quantity' => 0
+			]
+		)->increment('quantity');
 	}
 }
