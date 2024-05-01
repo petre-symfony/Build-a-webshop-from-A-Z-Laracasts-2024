6ebea78d43c1e6ef67230ea4250378a840a2f207
petrero <petrero@gmail.com>
2024-05-02 01:01:00 +0300

    25.6 So now that we have set everything up, we can move on and create the actual order and the order items.  So let's create a new order, and let's use our session information to fill out each of these columns. So this is all the information related to the amount that was charged, and the subtotal, tax, discount, shipping cost, etc. Next let's store the billing address. Now we can simply duplicate this array, rename this to shipping address, and update this reference from customer details to shipping details.

--- app/Actions/Webshop/HandleCheckoutSessionCompleted.php
+++ app/Actions/Webshop/HandleCheckoutSessionCompleted.php
@@ -9,5 +9,32 @@ class HandleCheckoutSessionCompleted {
 	public function handle($sessionId) {
 		$session = Cashier::stripe()->checkout->sessions->retrieve($sessionId);
 		$user = User::find($session->metadata->user_id);
+
+		$order = $user->orders()->create([
+			'stripe_checkout_session_id' => $session->id,
+			'amount_shipping' => $session->total_details->amount_shipping,
+			'amount_discount' => $session->total_details->amount_discount,
+			'amount_tax' => $session->total_details->amount_tax,
+			'amount_subtotal' => $session->amount_subtotal,
+			'amount_total' => $session->amount_total,
+			'billing_address' => [
+				'name' => $session->customer_details->name,
+				'city' => $session->customer_details->address->city,
+				'country' => $session->customer_details->address->country,
+				'line1' => $session->customer_details->address->line1,
+				'line2' => $session->customer_details->address->line2,
+				'postal_code' => $session->customer_details->address->postal_code,
+				'state' => $session->customer_details->address->state
+			],
+			'shipping_address' => [
+				'name' => $session->shipping_details->name,
+				'city' => $session->shipping_details->address->city,
+				'country' => $session->shipping_details->address->country,
+				'line1' => $session->shipping_details->address->line1,
+				'line2' => $session->shipping_details->address->line2,
+				'postal_code' => $session->shipping_details->address->postal_code,
+				'state' => $session->shipping_details->address->state
+			]
+		]);
 	}
 }
