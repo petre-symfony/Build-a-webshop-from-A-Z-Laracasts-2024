881961f4c6edb494b6ddcae0a39d1dd831f17030
petrero <petrero@gmail.com>
2024-05-02 10:07:07 +0300

    27.2 So let's head over to our editor, let's go into our checkout status component, let's set up our mount method, and let's set a property where we can store the session ID.  So now whenever we load this page, we'll set the session ID based on the request. We can verify that this is working by heading over to our blade file, and echoing the session ID. And there you go, that works.  So now let's create a computer property called getOrderProperty, and let's try and search for the order based on the session ID. Of course, let's make sure that we only check this on the actual current user to make it more secure, so in that case, to access this route, they need to be logged in, and they need to be the given user to which this session ID and order belongs. We can say orders, and we can say where checkoutSessionID is equal to this session ID.

--- app/Livewire/CheckoutStatus.php
+++ app/Livewire/CheckoutStatus.php
@@ -5,6 +5,16 @@ namespace App\Livewire;
 use Livewire\Component;
 
 class CheckoutStatus extends Component {
+	public $sessionId;
+
+	public function mount() {
+		$this->sessionId = request()->get('session_id');
+	}
+
+	public function getOrderProperty() {
+		return auth()->user()->orders()->where('stripe_checkout_session_id', $this->sessionId);
+	}
+
 	public function render() {
 		return view('livewire.checkout-status');
 	}
