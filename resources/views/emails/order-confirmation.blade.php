10c11ab39f6273c0d1659b3c88b617d8d8310fb8
petrero <petrero@gmail.com>
2024-05-02 11:57:29 +0300

    28.8 So let's switch back to our editor, and first let's go into our blade file, and let's remove all this HTML boilerplate, instead we'll use markdown for some of our elements, so we can reference a default component by using the add component directive, we'll enter the mail namespace, and we'll want to render the message components, all the way to the bottom, we will add the components, so now if we switch back to our browser and refresh, you'll get an error,   No hint path defined for [mail].  , this is because we have switched to a markdown style syntax, which is required in order for the components to render.

--- resources/views/emails/order-confirmation.blade.php
+++ resources/views/emails/order-confirmation.blade.php
@@ -1,104 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Order Confirmation #{{ $order->id }}</title>
-  </head>
-  <body>
-    <p>
-      Hey {{ $order->user->name }}
-    </p>
+@component('mail::message')
+  <p>
+    Hey {{ $order->user->name }}
+  </p>
 
-    <p>
-      Thank you for your order. You find all the details below
-    </p>
+  <p>
+    Thank you for your order. You find all the details below
+  </p>
 
-    <table>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Tax</th>
-          <th>Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($order->items as $item)
-          <tr>
-            <td>
-              {{ $item->name }} <br>
-              {{ $item->description }}
-            </td>
-            <td>
-              {{ $item->price }}
-            </td>
-            <td>
-              {{ $item->quantity }}
-            </td>
-            <td>
-              {{ $item->amount_tax }}
-            </td>
-            <td>
-              {{ $item->amount_total }}
-            </td>
-          </tr>
-        @endforeach
-      </tbody>
-      <tfoot>
-        @if($order->amount_shipping->isPositive())
-          <tr>
-            <td colspan="4" style="text-align: right">
-              Shipping Costs
-            </td>
-            <td>
-              {{ $order->amount_shipping }}
-            </td>
-          </tr>
-        @endif
-        @if($order->amount_discount->isPositive())
-          <tr>
-            <td colspan="4" style="text-align: right">
-              Discount
-            </td>
-            <td>
-              {{ $order->amount_discount }}
-            </td>
-          </tr>
-        @endif
-        @if($order->amount_tax->isPositive())
-          <tr>
-            <td colspan="4" style="text-align: right">
-              Tax
-            </td>
-            <td>
-              {{ $order->amount_tax }}
-            </td>
-          </tr>
-        @endif
-        @if($order->amount_subtotal->isPositive())
-          <tr>
-            <td colspan="4" style="text-align: right">
-              Subtotal
-            </td>
-            <td>
-              {{ $order->amount_subtotal }}
-            </td>
-          </tr>
-        @endif
-        @if($order->amount_total->isPositive())
-          <tr>
-            <td colspan="4" style="text-align: right">
-              Total
-            </td>
-            <td>
-              {{ $order->amount_total }}
-            </td>
-          </tr>
-        @endif
-      </tfoot>
-    </table>
-  </body>
-</html>
+  <table>
+  <thead>
+    <tr>
+      <th>Item</th>
+      <th>Price</th>
+      <th>Quantity</th>
+      <th>Tax</th>
+      <th>Total</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($order->items as $item)
+      <tr>
+        <td>
+          {{ $item->name }} <br>
+          {{ $item->description }}
+        </td>
+        <td>
+          {{ $item->price }}
+        </td>
+        <td>
+          {{ $item->quantity }}
+        </td>
+        <td>
+          {{ $item->amount_tax }}
+        </td>
+        <td>
+          {{ $item->amount_total }}
+        </td>
+      </tr>
+    @endforeach
+  </tbody>
+  <tfoot>
+    @if($order->amount_shipping->isPositive())
+      <tr>
+        <td colspan="4" style="text-align: right">
+          Shipping Costs
+        </td>
+        <td>
+          {{ $order->amount_shipping }}
+        </td>
+      </tr>
+    @endif
+    @if($order->amount_discount->isPositive())
+      <tr>
+        <td colspan="4" style="text-align: right">
+          Discount
+        </td>
+        <td>
+          {{ $order->amount_discount }}
+        </td>
+      </tr>
+    @endif
+    @if($order->amount_tax->isPositive())
+      <tr>
+        <td colspan="4" style="text-align: right">
+          Tax
+        </td>
+        <td>
+          {{ $order->amount_tax }}
+        </td>
+      </tr>
+    @endif
+    @if($order->amount_subtotal->isPositive())
+      <tr>
+        <td colspan="4" style="text-align: right">
+          Subtotal
+        </td>
+        <td>
+          {{ $order->amount_subtotal }}
+        </td>
+      </tr>
+    @endif
+    @if($order->amount_total->isPositive())
+      <tr>
+        <td colspan="4" style="text-align: right">
+          Total
+        </td>
+        <td>
+          {{ $order->amount_total }}
+        </td>
+      </tr>
+    @endif
+  </tfoot>
+</table>
+@endcomponent
