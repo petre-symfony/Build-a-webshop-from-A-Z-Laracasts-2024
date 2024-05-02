cd71fd63dec9951bf8549e7d5b099a1fed675398
petrero <petrero@gmail.com>
2024-05-02 21:49:32 +0300

    30.4 Now while we're here, let's move these two routes inside a route group that has the correct middleware supply that will ensure that these routes are only accessible whenever a user is logged in.  So we are going to take these two routes, and this is what we've defined earlier, which we pointed out, lots of uncommon keys. And inside this group, we'll put the routes that we created earlier. So this should still work. There we go. But now this is protected, so it requires you to be logged in. If we would open this in a new browser window and visit, you can see that we are redirected to the login page. Okay, let's create a simple order page where we can see the products that have been ordered, the billing and shipping information as well.

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', StoreFront::class)->name('home');
 Route::get('/product/{productId}', Product::class)->name('product');
 Route::get('/cart', Cart::class)->name('cart');
-Route::get('/checkout-status', CheckoutStatus::class)->name('checkout-status');
-Route::get('/order/{orderId}', ViewOrder::class)->name('view-order');
+
 Route::get('/preview', function (){
 	$order = Order::first();
 
 	return new OrderConfirmation($order);
 });
 
-//Route::middleware([
-//	'auth:sanctum',
-//	config('jetstream.auth_session'),
-//	'verified',
-//])->group(function () {
-//	Route::get('/dashboard', function () {
-//		return view('dashboard');
-//	})->name('dashboard');
-//});
+Route::middleware([
+	'auth:sanctum',
+	config('jetstream.auth_session'),
+	'verified',
+])->group(function () {
+	Route::get('/checkout-status', CheckoutStatus::class)->name('checkout-status');
+	Route::get('/order/{orderId}', ViewOrder::class)->name('view-order');
+});
