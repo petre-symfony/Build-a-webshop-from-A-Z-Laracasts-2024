c8e1927289f861b1b4aba4294a371189730a6eab
petrero <petrero@gmail.com>
2024-05-02 09:58:17 +0300

    27.1 Prepare the Order Confirmation Page  Now whenever a user completes his or her order, we want to show a thank you message, saying like, hey, thank you for your order, we received your payment. So let's head over to our Stripe checkout creation action and make some adjustments here to facilitate this.  So first, we're going to set a success URL, and our user will be redirected here after a successful payment. So we haven't had a route for this yet, so let's head over to our terminal, and run phpArtisan, like we're make, and create a new component called checkout.status.  Next, let's head over to our routes file, let's create a new route, and wire up a component. Now that we have our route defined, we can reference this here, and now there's an option for Stripe to pass along the session ID.  So what we can do here is append a custom ID to our string, and Stripe will replace this with the checkout session ID. So we can say question mark, and say session ID, and this is going to be equal to checkout session ID. And Stripe is familiar with this specific string, and once it sees that this is part of the success URL, this will be replaced with the checkout session ID.  Now while we're here, let's set up a cancel URL as well. So if a customer decides to exit out of the checkout process, they will be redirected here, and I think the most useful page is to simply redirect back to the cart. There we go.  So let's give this a try, let's add something to our cart, let's click checkout, now let's cancel out of the checkout session, and we are redirected back to the cart, so that works. Let's place a new order, and as you can see, we are redirected back to our checkout status route, and the session ID has been included by Stripe.

---
+++ app/Livewire/CheckoutStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CheckoutStatus extends Component {
+	public function render() {
+		return view('livewire.checkout-status');
+	}
+}
