62bd77d1a405053e4592e5972b966e7a3088da92
petrero <petrero@gmail.com>
2024-05-02 11:42:13 +0300

    28.6 Okay, so we have each of our items, we have the price, quantity, tax, total, and now we can have a total amount listed on the bottom.  So let's create a footer, and let's list all the data that we have, so if order amount and shipping is higher than zero, we'll say shipping cost, and return this value. Now we can repeat this, and do the same for amount discount, the total amount of tax, subtotal, and finally the total amount.  Now we take a look, you can see that we have the additional prices which are above zero, now these aren't formatted at this time, so let's head over to our order, and let's add these casts. There we go, so now if we head back, we get an error because this is now returning a money object instead of a number

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,7 +13,12 @@ class Order extends Model {
 
 	public $casts = [
 		'billing_address' => 'collection',
-		'shipping_address' => 'collection'
+		'shipping_address' => 'collection',
+		'amount_shipping' => MoneyCast::class,
+		'amount_discount' => MoneyCast::class,
+		'amount_tax' => MoneyCast::class,
+		'amount_subtotal' => MoneyCast::class,
+		'amount_total' => MoneyCast::class
 	];
 
 	public function items(): HasMany {
--- resources/views/emails/order-confirmation.blade.php
+++ resources/views/emails/order-confirmation.blade.php
@@ -47,6 +47,58 @@
           </tr>
         @endforeach
       </tbody>
+      <tfoot>
+        @if($order->amount_shipping>0)
+          <tr>
+            <td>
+              Shipping Costs
+            </td>
+            <td>
+              {{ $order->amount_shipping }}
+            </td>
+          </tr>
+        @endif
+        @if($order->amount_discount>0)
+          <tr>
+            <td>
+              Amount Discount
+            </td>
+            <td>
+              {{ $order->amount_discount }}
+            </td>
+          </tr>
+        @endif
+        @if($order->amount_tax>0)
+          <tr>
+            <td>
+              Amount Tax
+            </td>
+            <td>
+              {{ $order->amount_tax }}
+            </td>
+          </tr>
+        @endif
+        @if($order->amount_subtotal>0)
+          <tr>
+            <td>
+              Amount Subtotal
+            </td>
+            <td>
+              {{ $order->amount_subtotal }}
+            </td>
+          </tr>
+        @endif
+        @if($order->amount_total>0)
+          <tr>
+            <td>
+              Amount Total
+            </td>
+            <td>
+              {{ $order->amount_total }}
+            </td>
+          </tr>
+        @endif
+      </tfoot>
     </table>
   </body>
 </html>
