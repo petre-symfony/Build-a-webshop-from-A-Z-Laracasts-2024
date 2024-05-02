32b750e08c5883006ac08964f2702f76f3639633
petrero <petrero@gmail.com>
2024-05-02 11:16:20 +0300

    28.4 Okay, now these numbers, we need to make sure that we do the same as we did with our card, and make sure that these are automatically formatted whenever we echo out these numbers into our blade file.   So if we look at our card item, you can see what we did here, let's take a look at our product, for example, so we have price, so let's switch to our order, and let's define the accessor for our different columns. So we have price, this actually should work if we head back and refresh, there we go, perfect.

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Money\Money;
 
 class OrderItem extends Model {
 	use HasFactory;
+
+	protected function price():Attribute {
+		return Attribute::make(
+			get: function (int $value) {
+				return new Money($value, new \Money\Currency('USD'));
+			}
+		);
+	}
 }
