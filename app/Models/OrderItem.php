baa85b9490ccba8e211827e660a21d80ca75dfe8
petrero <petrero@gmail.com>
2024-05-02 11:29:27 +0300

    28.5 Additionally, we have amount discount, amount subtotal, amount text, and amount total.   Now this may become a bit repetitive because we have to repeat this for every column, like I mentioned, price, amount discount, amount subtotal, amount text, total, so maybe it's easier to just create a custom cast.  So if we open up our terminal, we can run php artisan make:cast MoneyCast.  So now we can simply define our cast array on our public property, and we can say price, and we're going to pass our money cast. So let's remove this, and let's head over to our custom casts, and here we have our getter, we'll paste in the code that we had earlier, and simply return it. And this will make it a bit easier, because now we can simply reference our different columns.  So if we switch back to our browser, you can see that all the values are now updated except total, we had amount, oh yeah, so we've got one amount total, there we go, now all the values are updated.  So this also means that we can actually clean up our product model as well, and apply the money cast here as well, and remove the protected method that we have here. Now we should move to cards, this is a bit different, so we don't need to make any changes here, let's take a look at card item, the subtotal, I can change and stay the same. So I don't think we need to make any additional changes.

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,11 +11,11 @@ use Money\Money;
 class OrderItem extends Model {
 	use HasFactory;
 
-	protected function price():Attribute {
-		return Attribute::make(
-			get: function (int $value) {
-				return new Money($value, new \Money\Currency('USD'));
-			}
-		);
-	}
+	protected $casts = [
+		'price' => MoneyCast::class,
+		'amount_discount' => MoneyCast::class,
+		'amount_tax' => MoneyCast::class,
+		'amount_subtotal' => MoneyCast::class,
+		'amount_total' => MoneyCast::class
+	];
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,13 +13,9 @@ use Money\Money;
 class Product extends Model {
 	use HasFactory;
 
-	protected function price():Attribute {
-		return Attribute::make(
-			get: function (int $value) {
-				return new Money($value, new \Money\Currency('USD'));
-			}
-		);
-	}
+	protected $casts = [
+		'price' => MoneyCast::class
+	];
 
 	public function variants(): HasMany {
 		return $this->hasMany(ProductVariant::class);
