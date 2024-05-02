93315ce1a23ae16f104b1d61b6c1fc1a212b4673
petrero <petrero@gmail.com>
2024-05-02 21:45:55 +0300

    30.3 So let's head back, and let's open up our View Order component. Let's create a mount method. This is going to pass the order ID.  Let's register the public property. Next, we're going to create a computed order property. So we'll say public function getOrderProperty, and we're going to scope it from the current login user. So this will prevent someone entering the ID inside the URL, which they don't have access to, and this is important. So we'll say orders, findorFail, and we'll pass the order ID.  So if we press the page, this should still work. And if we go to our blade file, we should be able to echo out the order ID, and that works.

--- app/Livewire/ViewOrder.php
+++ app/Livewire/ViewOrder.php
@@ -5,6 +5,16 @@ namespace App\Livewire;
 use Livewire\Component;
 
 class ViewOrder extends Component {
+	public $orderId;
+
+	public function mount($orderId) {
+		$this->orderId = $orderId;
+	}
+
+	public function getOrderProperty() {
+		return auth()->user()->orders()->findOrFail($this->orderId);
+	}
+
 	public function render() {
 		return view('livewire.view-order');
 	}
