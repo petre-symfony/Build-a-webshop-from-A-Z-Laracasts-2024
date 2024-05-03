9bc7a2a45a79e65d83889a0190adaee99ee9b1b2
petrero <petrero@gmail.com>
2024-05-03 12:53:49 +0300

    31.5 Okay, now we can continue and simply show each of the items which are in our order.  So to save some time, let's head over to our cards, and here we'll simply copy and paste our table, we'll add this here, and let's update this accordingly, so we have order items, and then we have our order name, sorry, our item name, and a description.  Let's add quantity to remove these two columns, we don't have a button either. This one we can remove as well, so we have product, quantity, and total. There we go, let's take a look.

--- resources/views/livewire/view-order.blade.php
+++ resources/views/livewire/view-order.blade.php
@@ -1,6 +1,34 @@
 <div class="grid grid-cols-2 gap-4">
   <x-panel class="mt-12 col-span-2" title="Your Order #{{ $this->order->id }}">
-    {{ $orderId }}
+    <table class="w-full">
+      <thead>
+      <tr>
+        <th class="text-left">Product</th>
+        <th class="text-left">Quantity</th>
+        <th class="text-right">Total</th>
+      </tr>
+      </thead>
+      <tbody>
+      @foreach($this->order->items as $item)
+        <tr>
+          <td>
+            {{ $item->name }} <br>
+            {{ $item->description }}
+          </td>
+          <td>{{ $item->quantity }}</td>
+
+          <td class="text-right">{{ $item->amount_total }}</td>
+        </tr>
+      @endforeach
+      </tbody>
+      <tfoot>
+      <tr>
+        <td colspan="2" class="text-right font-medium">Total</td>
+        <td class="font-medium text-right">{{ $this->order->amount_total }}</td>
+        <td></td>
+      </tr>
+      </tfoot>
+    </table>
   </x-panel>
 
   <x-panel class="col-span-1">
