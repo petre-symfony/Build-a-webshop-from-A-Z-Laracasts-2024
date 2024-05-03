400f16fc3de5e6abde80c0bdedb9b6a7e179d1a6
petrero <petrero@gmail.com>
2024-05-03 13:08:08 +0300

    31.9 Let's head over to our component, and let's add a computed property, which is going to be called getOrdersProperty, and this is going to return all the orders of our user.  Let's head to our blade file, and let's simply copy and paste one of the tables that we created earlier to save some time. We can remove the footer, and we'll update the columns, Order, Total, and let's add an OrderedAt column. So now we can loop through each of our orders. Here we're going to add our order ID.  We'll have Order, OrderedAt, and Total. And this is going to return a carbon object, so we can actually say div for humans to create a nicely formatted text line that will say, okay, you ordered this five days ago.  There you go, we have our Order, OrderedAdd, one day ago, and the total amount of our order. Let's make sure that we wrap this inside a panel. We'll add a title, My Orders. Perfect. Let's add a little bit of styling, so we'll add a maxWidth, large, and we'll center it on the page. And let's add a margin of 12. There we go.  Now let's make sure that we can actually click the order. We'll add a hashtag here and wrap this inside a anchor tag, and we'll add our route, which is ViewOrder, and we'll pass the order ID. And let's add an underline and make the font a little bit bolder. So now we can click to view all our order information.

--- app/Livewire/MyOrders.php
+++ app/Livewire/MyOrders.php
@@ -5,6 +5,10 @@ namespace App\Livewire;
 use Livewire\Component;
 
 class MyOrders extends Component {
+	public function getOrdersProperty() {
+		return auth()->user()->orders;
+	}
+
 	public function render() {
 		return view('livewire.my-orders');
 	}
--- resources/views/livewire/my-orders.blade.php
+++ resources/views/livewire/my-orders.blade.php
@@ -1,3 +1,28 @@
-<div>
-  {{-- Do your work, then step back. --}}
-</div>
+<x-panel title="My Orders" class="max-w-lg mx-auto mt-12">
+  <table class="w-full">
+    <thead>
+    <tr>
+      <th class="text-left">Order</th>
+      <th class="text-left">Ordered At</th>
+      <th class="text-right">Total</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($this->orders as $order)
+      <tr>
+        <td>
+          <a
+              href="{{ route('view-order', $order->id) }}"
+              class="underline font-medium"
+          >
+            #{{ $order->id }}
+          </a>
+        </td>
+        <td>{{ $order->created_at->diffForHumans() }}</td>
+
+        <td class="text-right">{{ $order->amount_total }}</td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
+</x-panel>
