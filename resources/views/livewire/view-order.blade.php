f71fc9cdcf01a4ccbb1ebf18ccc967fbc1a6ab2e
petrero <petrero@gmail.com>
2024-05-03 12:44:44 +0300

    31.3 Okay, let's continue with our order page. So first, let's set up a grid. Now let's make a grid that has two columns

--- resources/views/livewire/view-order.blade.php
+++ resources/views/livewire/view-order.blade.php
@@ -1,4 +1,6 @@
-<x-panel class="mt-12">
-  {{ $orderId }}
-  {{-- Care about people's approval and you will be their prisoner. --}}
-</x-panel>
+<div class="grid grid-cols-2">
+  <x-panel class="mt-12">
+    {{ $orderId }}
+    {{-- Care about people's approval and you will be their prisoner. --}}
+  </x-panel>
+</div>
